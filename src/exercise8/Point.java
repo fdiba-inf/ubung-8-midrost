395e409b8c03d4222ad9257d4053ae82099ce84e
Rostislav Dimov <91912721+midrost@users.noreply.github.com>
2021-12-02 11:48:06 +0200

    Initial implementation

--- src/exercise8/Point.java
+++ src/exercise8/Point.java
@@ -1,40 +1,56 @@
 package exercise8;
 
+import java.io.PipedOutputStream;
+
 public class Point {
     // Define attributes
+    double x;
+    double y;
 
     public Point() {
         // Initialize attributes to 0
+        this.x = 0;
+        this.y = 0;
     }
 
     public Point(double x, double y) {
         // Initialize attributes by coping x and y parameters
+        this.x = x;
+        this.y = y;
     }
 
     public Point(Point otherPoint) {
         // Initialize attributes by coping attributes of otherPoint
+        this.x = otherPoint.x;
+        this.y = otherPoint.y;
     }
 
     public void initialize() {
         // Change attributes through console
+        this.x = Utils.INPUT.nextDouble();
+        this.y = Utils.INPUT.nextDouble();
     }
 
     public void translate(double xDelta, double yDelta) {
         // Change attributes by adding xDelta and yDelta
+        this.x += xDelta;
+        this.y += yDelta;
     }
 
     public Point createNewTranslatedPoint(double xDelta, double yDelta) {
         // Create new point which is translated relative to this point
-        return null;
+        Point newPoint = new Point(this.x, this.y);
+        newPoint.translate(xDelta, yDelta);
+        return newPoint;
     }
 
     public boolean equals(Point otherPoint) {
         // Compare attributes of this point to attributes of otherPoint for equality
-        return false;
+        return Utils.equals(this.x, otherPoint.x) && Utils.equals(this.y, otherPoint.y);
     }
 
     public String toString() {
         // Represent attributes as string
-        return null;
+        return "(" + this.x + ", " + this.y + ")";
     }
 }
